d526ca19a0375f1156df484726c3f3b53e9ebdcd
Tyler Kontra <tyler@tylerkontra.com>
2019-02-26 23:09:03 -0800

    fix(test): uncomment

--- test/test_glove.py
+++ test/test_glove.py
@@ -5,7 +5,7 @@ corpus = b'test/resource/text8'
 vocab = b'test/output/vocab.txt'
 cooccur_bin = b'test/output/cooccur.bin'
 shuf_bin = b'test/output/cooccur.shuf.bin'
-# vocab_count.vocab_count(corpus, vocab, 1, 100, 100)
-# cooccur.cooccur(corpus, vocab, cooccur_bin, 
-#             2, 1, 15, b'test/output/tmp', 8.0)
+vocab_count.vocab_count(corpus, vocab, 1, 100, 100)
+cooccur.cooccur(corpus, vocab, cooccur_bin, 
+            2, 1, 15, b'test/output/tmp', 8.0)
 shuffle.shuffle(cooccur_bin, shuf_bin, b'test/output/shuf', 2, 2.2)
