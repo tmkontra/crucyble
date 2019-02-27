6c8f49b9e579ef6fad9dbd5ee5fe8fef01cb2381
Tyler Kontra <tyler@tylerkontra.com>
2019-02-26 22:52:55 -0800

    fix(memory): updates test script malloc

--- test/test_glove.py
+++ test/test_glove.py
@@ -8,4 +8,4 @@ shuf_bin = b'test/output/cooccur.shuf.bin'
 # vocab_count.vocab_count(corpus, vocab, 1, 100, 100)
 # cooccur.cooccur(corpus, vocab, cooccur_bin, 
 #             2, 1, 15, b'test/output/tmp', 8.0)
-shuffle.shuffle(cooccur_bin, shuf_bin, b'test/output/shuf', 2, 2.5)
+shuffle.shuffle(cooccur_bin, shuf_bin, b'test/output/shuf', 2, 2.2)
