cd459f5c5fb81a7bc27f56b4920b8e00016ad18f
Tyler M. Kontra <46075030+ttymck@users.noreply.github.com>
2019-03-01 20:03:13 -0800

    [WIP] feat(ci): add travis integration (#5)
    * feat(travis): devel version
    - trying to get builds to work
    
    * fix(travis): ignore matrix
    
    * fix(travis): poetry install
    
    * fix(travis): use xenial
    
    * fix(build): try different package prefix
    
    * fix(build): need init file in crucyble.lib
    
    * fix(build): typo
    
    * fix(travis): detect ci
    
    * fix(travis): twine upload
    
    * fix(build): builds with pypi description
    
    * fix(travis): use make in ci
    
    * chore(version): v0.1.0.dev2
    
    * feat(ci): deploy on tag
    
    * Revert "chore(version): v0.1.0.dev2"
    
    This reverts commit 1274077a3843d00fbb5f47ef6d9e78f4670e1a0b.

--- build.py
+++ build.py
@@ -1,7 +1,12 @@
+import os
 from distutils.extension import Extension
 from Cython.Build import cythonize
+import toml
 
-ext_pkg_prefix = "src.crucyble.lib."
+if os.getenv("CI"):
+    ext_pkg_prefix = "crucyble.lib"
+else:    
+    ext_pkg_prefix = "src.crucyble.lib."
 
 ext_sources = ("vocab_count", "cooccur", "shuffle", "glove")
 ext_source_path = "src/lib/"
@@ -14,10 +19,19 @@ extensions = [
 
 extensions = cythonize(extensions)
 
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+def pyproject_toml():
+    with open("pyproject.toml", "r") as f:
+        return toml.load(f)
+
 def build(setup_kwargs):
     """Needed for the poetry building interface."""
 
     setup_kwargs.update({
         'ext_modules' : extensions,
         'include_dirs' : ["src/lib"],
-})
+        'description': pyproject_toml()['tool']['poetry']['description'],
+        'long_description_content_type': 'text/markdown',
+    })
